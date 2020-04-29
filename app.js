6ff1bc149a6d3e0aebd4535114a6afea3e883aba
Georgiy Nefedov <georgiy.nefedov@tum.de>
2020-04-29 18:05:16 +0200

    test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var log = function(entry) {
 };
 
 var server = http.createServer(function (req, res) {
+    console.log("received: " + req.method);
+    console.log("rec: " + req.url)
     if (req.method === 'POST') {
         var body = '';
 
@@ -18,8 +20,10 @@ var server = http.createServer(function (req, res) {
         req.on('end', function() {
             if (req.url === '/') {
                 log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
+            } else if (req.url === '/scheduled') {
                 log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            } else if (req.url === '/addBot'){
+                console.log("adding bot");
             }
 
             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
--- funcs.js
+++ funcs.js
@@ -1,4 +1,24 @@
-function addBot() {
-    let name = document.getElementById("token").innerText;
+window.addBot = function(){
+
+    http = require('http')
+    http.request('ws://localhost/addBot',
+        {"method":"POST"},
+        (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+
 
 }
